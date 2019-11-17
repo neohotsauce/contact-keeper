70f285af0a535a5d042fc48970efa3d998ebe26e
neo_hotsauce <alihanyn@hotmail.com>
2019-11-18 01:59:12 +0500

    finished the users route for the backend

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,66 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check } = require("express-validator");
+const validateErrors = require("../helpers/validation");
 
 const router = express.Router();
+const User = require("../models/User");
 
-// @route   POST api/users
-// @desc    Register a user
-// @access  Public
-router.post("/", (req, res) => {
-  res.json("Register a user");
-});
+// POST - register a user   public
+router.post(
+  "/",
+  [
+    check("name", "Please add name")
+      .not()
+      .isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please eneter a password with 6 or more characters"
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    validateErrors(req, res);
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        res.status(400).json({ msg: "User already exists" });
+      }
+      user = new User({
+        name,
+        email,
+        password
+      });
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      return jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          return res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
